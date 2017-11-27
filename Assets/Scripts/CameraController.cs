b67610ba4a130f633bcb1b0e68ef96bd83600800
LZ-PC\lz <waly1029@126.com>
2017-11-28 00:59:54 +0900

    Player Movement & Camera Setting Created.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float pitch = 2f;
+
+    public float zoomSpeed = 4f;
+
+    public float minZoom = 3f;
+
+    public float maxZoom = 15f;
+
+    public float yawSpeed = 100f;
+
+    private float currentZoom = 10f;
+
+    private float currentYaw = 0f;
+
+	// Use this for initialization
+	void Update( ) {
+
+        currentZoom -= Input.GetAxis( "Mouse ScrollWheel" ) * zoomSpeed;
+
+        Debug.Log(currentZoom);
+
+        currentZoom = Mathf.Clamp( currentZoom, minZoom, maxZoom );
+
+        currentYaw += Input.GetAxis( "Horizontal" ) * yawSpeed * Time.deltaTime;
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ( ) {
+		
+        transform.position = target.position - offset * currentZoom;
+
+        transform.LookAt( target.position + Vector3.up * pitch );
+
+        transform.RotateAround( target.position, Vector3.up, currentYaw );
+        //void RotateAround(Vector3 point, Vector3 axis, float angle);
+        //围绕世界坐标的point点的axis旋转该变换angle度。
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
+    public LayerMask movementMask;
+
+    PlayerMotor playerMtor;
+
+    Camera camera;
 	// Use this for initialization
-	void Start () {
+	void Start ( ) {
 		
+        camera = Camera.main;
+
+        playerMtor = GetComponent<PlayerMotor>( );
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ( ) {
+
+        if ( Input.GetMouseButtonDown( 1 ) ) {
+
+            Ray ray = camera.ScreenPointToRay( Input.mousePosition );
+
+            RaycastHit hit;
+
+            if( Physics.Raycast( ray, out hit, 100, movementMask ) ) {
+
+                playerMtor.MoveToPoint( hit.point );
+
+                Debug.Log( hit.collider.name + hit.point );
+                //hitしたところにプレーヤーを移動させ
+                
+            }
+        }
+
+        if ( Input.GetMouseButtonDown( 0 ) ) {
+
+            Ray ray = camera.ScreenPointToRay( Input.mousePosition );
+
+            RaycastHit hit;
+
+            if( Physics.Raycast( ray, out hit, 100 ) ) {
+                //対話の可能性を確認、できるとフォーカスにする
+                
+            }
+        }
+
 	}
 }
